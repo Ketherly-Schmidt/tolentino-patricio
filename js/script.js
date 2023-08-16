951bfcf14def269bd39e3edc222097e140162d95
Ketherly Schmidt <57159441+Ketherly-Schmidt@users.noreply.github.com>
2023-08-16 08:36:31 -0300

    submenu

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
 
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+
+
+function menuShow() {
+    let mobileMenu = document.querySelector('.mobile-menu')
+    if (mobileMenu.classList.contains('open')){
+        mobileMenu.classList.remove('open');
+        document.querySelector('.icon').src = "image/menu.svg"
+    }
+
+    else{
+        mobileMenu.classList.add('open')
+        document.querySelector('.icon').src = "image/exit.svg"
+    }
+}
